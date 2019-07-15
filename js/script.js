e59735e344929c6074d312551e41ce88cda4d494
nawaz <nawazsharif.csm@gmail.com>
2019-07-15 18:54:46 +0600

    1st push

---
+++ js/script.js
@@ -0,0 +1,42 @@
+function nav_closeFunction() {
+    $('#show_menu').removeClass('menu-show');
+}
+
+function nav_openFunction() {
+    $('#show_menu').addClass('menu-show');
+}
+
+$(document).ready(function($) {
+    "use strict";
+    //  TESTIMONIALS CAROUSEL HOOK
+    $('#customers-testimonials').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots: true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1170: {
+                items: 3
+            }
+        }
+    });
+});
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+});
